2566899383ea62491512a16a85cf752b4af024f5
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2023-08-20 14:31:18 +0600

    upload all file

---
+++ pharmacy/admin/includes/header.php
@@ -0,0 +1,11 @@
+<header class="navbar navbar-dark sticky-top" style="background-color: #FFC38C;">
+    <a href="index.php"> <img src="../images/logo copy.png" style="height: 60px; display: inline; padding-bottom: 5px; width:250px; margin: 0px 40px"></a>
+
+    <div class="navbar-nav">
+        <div class="nav-item text-nowrap">
+            <a class="nav-link px-3" href="logout.php">Sign out</a>
+        </div>
+
+    </div>
+
+</header>
